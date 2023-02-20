306776dff4d466ba8e5a25b0017bbfac3fead43f
root <root@MSI.localdomain>
2023-02-20 13:16:26 +0100

    Product page
    Show correct product

--- src/index.php
+++ src/index.php
@@ -29,14 +29,14 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a href="" class="navbar-brand navbar-left"><img src="img/header-icon.jpeg"></a>
+                    <a href="" class="navbar-brand navbar-left"><img src="Images/header-icon.jpeg"></a>
                 </div>
                 <!---Collapsable navbar--->
                 <div class="collapse navbar-collapse" id="page_header">
                     <!--Navigation buttons-->
                     <div class="navbar-nav navbar-left">
                         <ul class="nav navbar-nav">
-                            <li class="active"><a href="index.html">Home <span class="sr-only">(current)</a></li>
+                            <li class="active"><a href="index.php">Home <span class="sr-only">(current)</a></li>
                             <li class="inactive"><a href="about.html">About <span class="sr-only">(current)</a></li>
                         </ul>
                     </div>
@@ -60,5 +60,33 @@
             </div>
         </nav>
 
+        <?php
+            ini_set('display_errors', 1);
+            $user="admin";
+            $password="1234";
+            $database="stonebase";
+            $server="localhost";
+
+            $conn = new mysqli($server, $user, $password, $database);
+            if($conn->connect_error){
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "SELECT * FROM product";
+
+            $result = $conn->query($sql);
+
+            echo '<table class="table table-bordered table-responsive" cellpadding="0">';
+            while($row = $result->fetch_assoc()) {
+                echo '<tr>
+                        <td style="width: 10%">' . '<img src="'.$row['image'].'">' . '</td>
+                        <td><h1>' . $row['name'] . '</h1><hr>
+                        <h3>' . $row['description'] . '</h3></td>
+                        <td style="width:20%; vertical-allign: middle; text-allign: center;"><h2>' . $row['price'] . ' kr</h2>
+                        <h2>' . $row['stock'] . ' in stock</h2></td>
+                    </tr>';
+                
+            };
+        ?>
     </body>
 </html>
