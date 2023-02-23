5fc32fa8a0ad57d21d71f7072feda5e589be8850
Emil Lock <emil.loock@gmail.com>
2023-02-23 15:45:55 +0100

    Products now on restock page
    To be able to see anything on the
    page aside from the navbar you need
    to be logged in as an employee.
    
    If logged in as an employee the
    products are displayed on the page
    with input fields that will be used
    to edit price and stock (not implemented).
    
    Issue 17

--- src/restock_page.php
+++ src/restock_page.php
@@ -1,3 +1,6 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 
 <html>
@@ -20,6 +23,7 @@
     <body>
         <!---Navbar--->
         <nav class="navbar navbar-inverse navbar-fixed-top">
+	  <a class="navbar-brand" href="">MemeStones</a>
             <div class="container"> <!---make "container-fluid" if needed more space--->
                 <!---Make collapsable--->
                 <div class="navbar-header">
@@ -29,7 +33,6 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a href="" class="navbar-brand navbar-left"><img src="img/header-icon.jpeg"></a>
                 </div>
                 <!---Collapsable navbar--->
                 <div class="collapse navbar-collapse" id="page_header">
@@ -59,6 +62,74 @@
                 </div>
             </div>
         </nav>
+<?php
+
+if(!isset($_SESSION["u_id"])) {
+	echo '<div class="container" style="padding-top:50px;">
+		<h1 style="align: center;"> YOU ARE NOT AN EMPLOYEE </h1>
+		</div>';
+		die("Not logged in as an employee");
+}else{
+
+	/*Connect to db*/
+	$user = "admin";
+	$password="1234";
+	$database="stonebase";
+	$server="localhost";
+
+	$conn = new mysqli($server, $user, $password, $database);
+	if($conn->connect_error){
+		die("Connection to database failed: " . $conn->connect_error);
+	}
+
+	$u_id = $_SESSION["u_id"];
+	$sql = "SELECT * FROM employee WHERE u_id='$u_id'";
+
+	$res = $conn->query($sql);
+
+	if(mysqli_num_rows($res) == 0) {
+		echo '<div class="container" style="padding-top:50px;">
+			<h1 style="align: center;"> YOU ARE NOT AN EMPLOYEE </h1>
+			</div>';
+		die("Not logged in as an employee");
+	}
+}
+?>
+
+	<!-- Logged in as employee here -->
+	<div class="container" style="padding-top:100px;">
+	    <div class="panel panel-default">
+		<div class="panel-body">
+		<form>
+<?php
+
+$sql = "SELECT * FROM product";
+
+$res = $conn->query($sql);
+
+
+echo '<table class="table table-bordered table-responsive table-hover table-cursor" cellpadding="0">';
+while($row = $res->fetch_assoc()) {
+	echo '<tr onclick="goto_product_page(' . $row['p_id'] . ')">
+		<td style="width: 10%">' . '<img src="'.$row['image'].'">' . '</td>
+		<td><h1>' . $row['name'] . '</h1><hr>
+		<h3>' . $row['description'] . '</h3></td>
+		<td style="width:20%; vertical-align: middle; text-align: center;">
+		<div class="form-group" id="price_'.$row['p_id'].'">
+			<h3>Price:</h3>
+			<input type="number" class="form-control" placeholder="'.$row['price'].'"
+		</div>
+		<div class="form-group" id="stock_'.$row['p_id'].'">
+			<h3>Stock:</h3>
+			<input type="number" class="form-control" placeholder="'.$row['stock'].'"
+		</div>
+		</tr>';
+};
+?>
+			</form>
+			</div>
+		</div>
+	</div>
 
     </body>
 </html>
