964f2f9175ac04d3b682b2cda0b44a913eb30076
Emil Lock <emiloc-0@student.ltu.se>
2023-03-03 13:23:22 +0000

    Updated the database backup
    Done as part of 13, 14, 15 & 33

--- database_backup/stonebase_backup.sql
+++ database_backup/stonebase_backup.sql
@@ -72,34 +72,55 @@ INSERT INTO `employee` VALUES (0,100000,1,'Elon','Musk'),(1,5,0,'Bob','Bob');
 UNLOCK TABLES;
 
 --
--- Table structure for table `order`
+-- Table structure for table `order_item`
 --
 
-DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `order_item`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order` (
+CREATE TABLE `order_item` (
   `o_id` int NOT NULL,
-  `u_id` int NOT NULL,
   `p_id` int NOT NULL,
-  `order_date` timestamp NOT NULL,
   `quantity` int NOT NULL,
   PRIMARY KEY (`o_id`),
-  UNIQUE KEY `o_id_UNIQUE` (`o_id`),
-  KEY `u_id_idx` (`u_id`),
-  KEY `p_id_idx` (`p_id`),
-  CONSTRAINT `p_id_order` FOREIGN KEY (`p_id`) REFERENCES `product` (`p_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `u_id_order` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `o_id` FOREIGN KEY (`o_id`) REFERENCES `orders` (`o_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order`
+-- Dumping data for table `order_item`
 --
 
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+LOCK TABLES `order_item` WRITE;
+/*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `o_id` int NOT NULL AUTO_INCREMENT,
+  `u_id` int NOT NULL,
+  `order_date` timestamp(2) NOT NULL,
+  PRIMARY KEY (`o_id`),
+  UNIQUE KEY `o_id_UNIQUE` (`o_id`),
+  KEY `u_id_idx` (`u_id`),
+  CONSTRAINT `u_id` FOREIGN KEY (`u_id`) REFERENCES `user` (`u_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -110,14 +131,15 @@ DROP TABLE IF EXISTS `product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
-  `p_id` int NOT NULL,
+  `p_id` int NOT NULL AUTO_INCREMENT,
   `price` int NOT NULL,
   `stock` int NOT NULL,
   `name` varchar(32) NOT NULL,
   `description` varchar(256) NOT NULL,
   `image` varchar(256) DEFAULT NULL,
   PRIMARY KEY (`p_id`),
-  UNIQUE KEY `p_id_UNIQUE` (`p_id`)
+  UNIQUE KEY `p_id_UNIQUE` (`p_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -127,7 +149,6 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (0,100,10,'Gigachad','giga','Images/gigachad.jpg'),(1,50,30,'Hello there','obi','Images/hellothere.jpg'),(2,150,5,'Doge','dog','Images/doge.jpg');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,14 +160,16 @@ DROP TABLE IF EXISTS `shopping_cart`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `shopping_cart` (
+  `entry` int NOT NULL AUTO_INCREMENT,
   `c_id` int NOT NULL,
   `p_id` int NOT NULL,
   `quantity` int NOT NULL,
-  PRIMARY KEY (`c_id`),
-  UNIQUE KEY `c_id_UNIQUE` (`c_id`),
+  PRIMARY KEY (`entry`),
+  UNIQUE KEY `entry_UNIQUE` (`entry`),
+  KEY `c_id_idx` (`c_id`),
   KEY `p_id_idx` (`p_id`),
-  CONSTRAINT `c_id_cart` FOREIGN KEY (`c_id`) REFERENCES `user` (`u_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `p_id_cart` FOREIGN KEY (`p_id`) REFERENCES `product` (`p_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+  CONSTRAINT `c_id` FOREIGN KEY (`c_id`) REFERENCES `user` (`u_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `p_id` FOREIGN KEY (`p_id`) REFERENCES `product` (`p_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -182,7 +205,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (0,'manager','test_password'),(1,'Bob','1234'),(2,'Ted','123');
+INSERT INTO `user` VALUES (0,'manager','test_password'),(1,'Bob','1234'),(2,'Ted','123'),(3,'Adam','12345');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -195,4 +218,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-14 13:21:36
+-- Dump completed on 2023-03-03 13:21:32
